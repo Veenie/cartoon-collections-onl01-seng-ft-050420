82ea01ca6fc671522cd33139cfaa8fa95d034255
Raveen K Sharma <raveen08@gmail.com>
2020-05-10 22:55:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -33,4 +33,9 @@ def long_planeteer_calls# code an argument here
   end 
 end
 
-
+def find_the_cheese(array)
+  cheese_types = ["cheddar", "gouda", "camembert"]
+    array.find do |type|
+    cheese_types.include?(type)
+  end 
+end
